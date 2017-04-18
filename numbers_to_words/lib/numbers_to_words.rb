64fac2c13e6bd9d0e9a6c207ed684cadc27c51fb
Niklas Long and Tanner Eustice <niklas@>
2017-04-18 15:00:10 -0700

    add 4 passing specs

--- numbers_to_words/lib/numbers_to_words.rb
+++ numbers_to_words/lib/numbers_to_words.rb
@@ -25,6 +25,7 @@ class String
     }
 
     second_digit = {
+      "0"  => "ten",
       "2"  => "twenty",
       "3"  => "thirty",
       "4"  => "fourty",
@@ -35,22 +36,79 @@ class String
       "9"  => "ninety"
     }
 
-    input_array = self.split('')
+    input_array = self.reverse.scan(/.{1,3}/)
     number_word = ""
 
-    input_array.each_with_index do |number, index|
-      if index.eql?(input_array.length - 1)
-        number_word << first_digit.fetch(number)
-      elsif index.eql?(input_array.length - 2)
-        if input_array.at(input_array.length - 2).eql?("1")
-          number_word << tens.fetch(number)
-        else
-          number_word << "#{second_digit.fetch(number)} "
+    input_array.each_with_index do |number_group, group_index|
+
+      char_trio = number_group.split("")
+      print char_trio
+      number_place = ""
+
+      if group_index == 0
+        number_place = ""
+      elsif group_index == 1
+        number_place = "thousand"
+      elsif group_index == 2
+        number_place = "million"
+      elsif group_index == 3
+        number_place = "billion"
+      elsif group_index == 7
+        number_place = "trillion"
+      end
+
+      number_word.prepend(" #{number_place} ")
+
+      char_trio.each_with_index do |number, number_index|
+        if number_index == 0
+          number_word.prepend(first_digit.fetch(number))
+        elsif number_index == 2
+          number_word.prepend("#{first_digit.fetch(number)} hundred ")
+        elsif number_index == 1
+          number_word.prepend("#{second_digit.fetch(number)} ")
         end
-      elsif index.eql?(input_array.length - 3)
-        number_word << "#{first_digit.fetch(number)} hundred "
       end
     end
+    number_word.slice!((number_word.length - 2)..number_word.length)
     number_word
   end
 end
+
+
+
+
+
+
+
+
+
+    # input_array.each_with_index do |number, index|
+    #   if index.eql?(input_array.length - 1)
+    #     number_word << first_digit.fetch(number)
+    #   elsif index.eql?(input_array.length - 2)
+    #     if input_array.at(input_array.length - 2).eql?("1")
+    #       if input_array.at(input_array.length - 1).eql?("0")
+    #         number_word << second_digit.fetch(input_array.at(-1))
+    #         break
+    #       else
+    #         number_word << tens.fetch(input_array.at(-1))
+    #         break
+    #       end
+    #     else
+    #       number_word << "#{second_digit.fetch(number)} "
+    #     end
+    #   elsif index.eql?(input_array.length - 3)
+    #     number_word << "#{first_digit.fetch(number)} hundred "
+    #   elsif index.eql?(input_array.length - 4)
+    #     number_word << "#{first_digit.fetch(number)} thousand "
+    #   elsif index.eql?(input_array.length - 5)
+    #     if input_array.at(input_array.length - 5).eql?("1")
+    #       if input_array.at(input_array.length - 4).eql?("0")
+    #         number_word << second_digit.fetch(input_array.at(-1))
+    #       else
+    #         number_word << tens.fetch(input_array.at(input_array.length - 4))
+    #       end
+    #     end
+    #   end
+    # end
+    # number_word
--- numbers_to_words/spec/numbers_to_words_spec.rb
+++ numbers_to_words/spec/numbers_to_words_spec.rb
@@ -5,13 +5,22 @@ describe("String#numbers_to_words") do
   it("if input is 1, return one") do
     expect("1".numbers_to_words()).to(eq("one"))
   end
-  it("if input is 15, return twenty-five") do
-    expect("25".numbers_to_words()).to(eq("twenty five"))
-  end
+  # it("if input is 10, return ten") do
+  #   expect("10".numbers_to_words()).to(eq("ten"))
+  # end
+  # it("if input is 15, return fifteen") do
+  #   expect("15".numbers_to_words()).to(eq("fifteen"))
+  # end
   it("if input is 25, return twenty-five") do
     expect("25".numbers_to_words()).to(eq("twenty five"))
   end
-  it("if input is 537, return twenty-five") do
-    expect("537".numbers_to_words()).to(eq("five hundred thirty seven"))
+  it("if input is 999, return nine hundred ninety nine") do
+    expect("999".numbers_to_words()).to(eq("nine hundred ninety nine"))
   end
+  it("if input is 7925, return seven thousand nine hundred twenty five") do
+    expect("7925".numbers_to_words()).to(eq("seven thousand nine hundred twenty five"))
+  end
+  # it("if input is 17915, return seventeen thousand nine hundred fifteen") do
+  #   expect("17915".numbers_to_words()).to(eq("seventeen thousand nine hundred fifteen"))
+  # end
 end
